8874581ccd5418c649fbe7c8086b29c5d590d4ab
MichaelVoiun <supermishka3@gmail.com>
2022-09-03 19:49:05 +0300

    Первый коммит

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Домашнее задание урока 1.7. Строки");
+        System.out.println();
+        System.out.println("Задание 1");
+        String firstName = "Ivan ";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov ";
+        String fullName = lastName + firstName + middleName;
+        System.out.println(fullName);
+        System.out.println();
+        System.out.println("Задание 2");
 
     }
 }
